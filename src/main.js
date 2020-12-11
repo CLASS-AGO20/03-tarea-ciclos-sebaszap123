200d6b8a630458222de3b4fb4a76c9746a8b66bc
sebaszap123 <mfrausto0@ucol.mx>
2020-12-10 20:58:21 -0600

    Added function sumatoriaSerieDos and tested it

--- src/main.js
+++ src/main.js
@@ -6,12 +6,26 @@ export default class App {
     }
     return suma;
   }
+
+    sumatoriaSerieDos(numero) {
+    let suma = 0;
+    let i = 1;
+    while (i <= numero) {
+      if ((i != 1 && i % 2 != 0)) {
+        suma = suma - 1 / i;
+      } else if (i % 2 === 0 || i == 1) {
+        suma = suma + 1 / i;
+      }
+      i++;
+    }
+    return suma;
+  }
 }
 
 let app = new App();
 
 console.log(app.sumatoriaSerieUno(2));
 
-/* console.log(app.sumatoriaSerieDos(2));
+console.log(app.sumatoriaSerieDos(2));
 console.log(app.sumatoriaSerieDos(3));
 console.log(app.sumatoriaSerieDos(7)); 
