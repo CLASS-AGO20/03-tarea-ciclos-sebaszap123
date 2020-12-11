c490a6a25015a37761341a6fdb8cd07a69d51dca
sebaszap123 <mfrausto0@ucol.mx>
2020-12-10 20:57:20 -0600

    Added function sumatoriaSerieUno and tested

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-export default class App {}
+export default class App {
+  sumatoriaSerieUno(numero) {
+    let suma = 0;
+    for (let i = 1; i <= numero; i++) {
+      suma = suma + 1 / i;
+    }
+    return suma;
+  }
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(2));
+
+/* console.log(app.sumatoriaSerieDos(2));
+console.log(app.sumatoriaSerieDos(3));
+console.log(app.sumatoriaSerieDos(7)); 
